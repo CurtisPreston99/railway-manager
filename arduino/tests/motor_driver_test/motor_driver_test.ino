a929bdda2ca8c2bf172b8eb598c30094f88142e9
wizordgamming <wizordgamming@gmail.com>
2023-03-12 18:02:52 +1300

    rail way motor driver tested and working

--- arduino/tests/motor_driver_test/motor_driver_test.ino
+++ arduino/tests/motor_driver_test/motor_driver_test.ino
@@ -8,12 +8,14 @@
 #define in2 7
 #define button 4
 
-int rotDirection = 0;
+bool rotDirection = false;
 
-int speed = 0;
+int speed = 1000;
 bool speedIncress = true;
 
 void setup() {
+  Serial.begin(9600); // open the serial port at 9600 bps:
+
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -30,26 +32,26 @@ void loop() {
     speedIncress = false;
   }
 
-  if (speed <= 0) {
+  if (speed <= 400) {
     speedIncress = true;
-    rotDirection == 0 ? rotDirection = 1 : rotDirection = 0;
+    rotDirection = !rotDirection;
   }
 
   int pwmOutput = map(potValue, 0, 1023, 0, 255);  // Map the potentiometer value from 0 to 255
   analogWrite(enA, pwmOutput);                     // Send PWM signal to L298N Enable pin
-
+//
   // If button is pressed - change rotation direction
-  if (rotDirection == 0) {
+  if (!rotDirection) {
     digitalWrite(in1, HIGH);
     digitalWrite(in2, LOW);
-    rotDirection = 1;
     delay(20);
   }
   // If button is pressed - change rotation direction
-  if (rotDirection == 1) {
+  if (rotDirection) {
     digitalWrite(in1, LOW);
     digitalWrite(in2, HIGH);
-    rotDirection = 0;
     delay(20);
   }
+
+  Serial.println(speed);
 }
